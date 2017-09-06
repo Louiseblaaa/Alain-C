f45096e9a253400cb2c8e8fe27cc2b462f236392
Louise Mortelette <louizemortelette@gmail.com>
2017-09-06 10:37:07 +0200

    new controller Themes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'themes/index'
+
+  get 'themes/show'
+
+  get 'themes/new'
+
+  get 'themes/create'
+
+  get 'themes/edit'
+
+  get 'themes/update'
+
+  get 'themes/destroy'
+
   get 'pages/home'
 
   get 'pages/search'
