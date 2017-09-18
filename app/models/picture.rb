07b78342ba1e11858c6649b4dee612d9956b11c4
Louise Mortelette <louizemortelette@gmail.com>
2017-09-18 14:41:13 +0200

    modif models & uploaders

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,10 +1,13 @@
 class Picture < ApplicationRecord
+
+  mount_uploader :photo, PhotoUploader
+
   belongs_to :theme
   has_many :reviews, dependent: :destroy
 
   validates :title
   validates :description, presence: true
-  validates :photo, presence: true
+  validates :photo
   validates :created_at, presence: true
   validates :updated_at
 end
