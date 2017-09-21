722bea483bef33dd1229a787f5e68cbb8f5b36e7
Louise Mortelette <louizemortelette@gmail.com>
2017-09-21 10:30:41 +0200

    ajout seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Theme.destroy_all
+Picture.destroy_all
+puts 'Creating themes'
+Theme.create!(title: 'Fête de Gayant', description: 'défilé 2016')
